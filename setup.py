07ea6150390ef0243688e9823755450e9768c8bc
limeng <limeng@moviewisdom.cn>
2017-05-11 18:10:51 +0800

    add new setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dataobj',
+    version='1.0',
+    packages=['dataobj', 'dataobj.sqlargs'],
+    url='',
+    license='Apache',
+    author='Christopher Lee',
+    author_email='',
+    description='Simple ORM package.'
+)
