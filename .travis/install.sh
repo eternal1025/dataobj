3d95c1dbf158be1586cab1c612345bb2ad7fa0c1
0xE8551CCB <limeng@moviewisdom.cn>
2017-07-26 16:38:14 +0800

    fix travis config failed

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,10 +1,10 @@
 # Install dependency: `pymysqlpool`
-git clone --branch=master https://github.com:0xE8551CCB/pymysqlpool.git
-python pymysqlpool/setup.py install
+git clone --depth=50 --branch=master https://github.com/0xE8551CCB/pymysqlpool.git dependencies/pymysqlpool
+python dependencies/pymysqlpool/setup.py install
 
 # Install dependency: `dbutil`
-git clone --branch=master https://github.com/0xE8551CCB/dbutil.git
-python dbutil/setup.py install
+git clone --depth=50 --branch=master https://github.com/0xE8551CCB/dbutil.git dependencies/dbutil
+python dependencies/dbutil/setup.py install
 
 # Install other dependencies
 pip install pandas
