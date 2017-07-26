0c8804f4f0dec6b9e4048aa6351b873d25208918
0xE8551CCB <limeng@moviewisdom.cn>
2017-07-26 16:44:21 +0800

    use pip to install packages from github

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,10 +1,8 @@
 # Install dependency: `pymysqlpool`
-git clone --depth=50 --branch=master https://github.com/0xE8551CCB/pymysqlpool.git dependencies/pymysqlpool
-python dependencies/pymysqlpool/setup.py install
+pip install git+https://github.com/0xE8551CCB/pymysqlpool.git
 
 # Install dependency: `dbutil`
-git clone --depth=50 --branch=master https://github.com/0xE8551CCB/dbutil.git dependencies/dbutil
-python dependencies/dbutil/setup.py install
+pip install git+https://github.com/0xE8551CCB/dbutil.git
 
 # Install other dependencies
 pip install pandas
