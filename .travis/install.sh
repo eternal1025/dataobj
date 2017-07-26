4c368aae30e084c62e2a2a5a0d10e04b400eef21
0xE8551CCB <limeng@moviewisdom.cn>
2017-07-26 16:22:20 +0800

    add new shell scripts to install dependencies

---
+++ .travis/install.sh
@@ -0,0 +1,11 @@
+# Install dependency: `pymysqlpool`
+git clone --branch=master git@github.com:0xE8551CCB/pymysqlpool.git
+python pymysqlpool/setup.py install
+
+# Install dependency: `dbutil`
+git clone --branch=master git@github.com:0xE8551CCB/dbutil.git
+python dbutil/setup.py install
+
+# Install other dependencies
+pip install pandas
+pip install pymysql
